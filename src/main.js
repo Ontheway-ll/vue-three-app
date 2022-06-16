b99c8c024a30fc416983bc60a8b8c7b4c886bdb5
yxliulei <mx_liulei@163.com>
2022-06-16 11:39:29 +0800

    全局添加过滤器

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-import Vant from 'vant'
 import 'vant/lib/index.css'
-createApp(App).use(store).use(router).use(Vant).mount('#app')
+const app = createApp(App)
+app.use(store).use(router).mount('#app')
+// 全局过滤器
+app.config.globalProperties.$filters={
+    prefix(url){
+     if (url&&url.startWith('http')) {
+         return url
+     } else {
+         url=`http://backend-api-01.newbee.ltd${url}`
+         return url
+     }
+    }
+}
